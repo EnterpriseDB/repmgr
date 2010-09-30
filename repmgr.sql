72fe188b1cf01101f14d9914f48935781d756c8a
Jaime Casanova <jaime@2ndQuadrant.com>
2010-09-30 10:44:13 -0500

    Fix sql script, specially the view that was lacking of an alias Reported and patch by: Cedric Villemain

--- repmgr.sql
+++ repmgr.sql
@@ -1,22 +1,20 @@
-drop table if exists repl_nodes;
-
 /*
  * The table repl_nodes keeps information about all machines in
  * a cluster
  */
+drop table if exists repl_nodes cascade;
 CREATE TABLE repl_nodes (
   id 		integer	primary key,
   cluster   text	not null,	-- Name to identify the cluster
   conninfo	text	not null 	
 );
 
-drop table if exists repl_status;
-
 /*
- * Keeps monitor info about every node and their "position" relative 
+ * Keeps monitor info about every node and their relative "position" 
  * to primary
  */
-CREATE TABLE repl_monitor(
+drop table if exists repl_monitor cascade;
+CREATE TABLE repl_monitor (
   primary_node                   INTEGER NOT NULL,
   standby_node                   INTEGER NOT NULL,
   last_monitor_time		    	 TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -30,8 +28,9 @@ CREATE TABLE repl_monitor(
 /*
  * A useful view 
  */
+drop view if exists repl_status;
 CREATE VIEW repl_status AS
 SELECT *, now() - (select max(last_monitor_time) from repl_monitor b
                     where b.primary_node = a.primary_node
                       and b.standby_node = a.standby_node)
-  FROM repl_monitor;
+  FROM repl_monitor a;
