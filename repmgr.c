9b7a078e0ec852a0de31271c017bc8e5bb8d2007
Gabriele Bartolini <gabriele.bartolini@2ndQuadrant.it>
2010-12-16 21:50:15 +0100

    Added license information in every source file

--- repmgr.c
+++ repmgr.c
@@ -1,14 +1,27 @@
 /*
- * repmgr.c
- * Copyright (c) 2ndQuadrant, 2010
+ * repmgr.c - Command interpreter for the repmgr
+ * Copyright (C) 2ndQuadrant, 2010
  *
- * Command interpreter for the repmgr
  * This module is a command-line utility to easily setup a cluster of
  * hot standby servers for an HA environment
  * 
  * Commands implemented are.
  * MASTER REGISTER, STANDBY REGISTER, STANDBY CLONE, STANDBY FOLLOW, 
  * STANDBY PROMOTE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 
 #include "repmgr.h"
