235903e517fe9f1db61bfaebdd633a4424b3481f
Cédric Villemain <cedric@2ndQuadrant.fr>
2011-03-28 18:37:41 +0200

    Fix rsync return code test

--- repmgr.c
+++ repmgr.c
@@ -1485,7 +1485,7 @@ copy_remote_files(char *host, char *remote_user, char *remote_path,
 	 * List of ignorable rsync errors:
 	 * 24     Partial transfer due to vanished source files
 	 */
-	if ((r == 24) && is_directory)
+	if ((WEXITSTATUS(r) == 24) && is_directory)
 	{
 		if (!runtime_options.ignore_rsync_warn)
 			log_warning( _("\nrsync completed with return code 24 "
@@ -1498,7 +1498,6 @@ copy_remote_files(char *host, char *remote_user, char *remote_path,
 		else
 			r = 0;
 	}
-
 	if (r != 0)
 		log_err(_("Can't rsync from remote file or directory (%s:%s)\n"),
 		        host_string, remote_path);
