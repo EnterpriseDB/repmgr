EnterpriseDB/repmgr

commit cb764b180dece2f3bf33543712542c49cb1ef704

Let the clone happen in a session with synchronous_commit off. This is because in pg 9.1 the default configuration can easily allow sync rep to be activated even if no standby is present and will block pg_start_backup() and pg_stop_backup() in that case.

repmgr.c
@@ -896,6 +896,20 @@ do_standby_clone(void)
 
 	log_notice(_("Starting backup...\n"));
 
+	/* 
+	 * in pg 9.1 default is to wait for a sync standby to ack, 
+	 * avoid that by turning off sync rep for this session
+	 */
+	sqlquery_snprintf(sqlquery, "SET synchronous_commit TO OFF)");
+	res = PQexec(conn, sqlquery);
+	if (PQresultStatus(res) != PGRES_COMMAND_OK)
+	{
+		log_err("Can't set synchronous_commit: %s\n", PQerrorMessage(conn));
+		PQclear(res);
+		PQfinish(conn);
+		exit(ERR_BAD_CONFIG);
+	}
+
 	/*
 	 * inform the master we will start a backup and get the first XLog filename
 	 * so we can say to the user we need those files

commit 0240cb2bfb9503b3d384b3373e96b2bcfc1b896e

Fix a typo introduced in commit cb764b180dece2f3bf33543712542c49cb1ef704

repmgr.c
@@ -900,7 +900,7 @@ do_standby_clone(void)
 	 * in pg 9.1 default is to wait for a sync standby to ack, 
 	 * avoid that by turning off sync rep for this session
 	 */
-	sqlquery_snprintf(sqlquery, "SET synchronous_commit TO OFF)");
+	sqlquery_snprintf(sqlquery, "SET synchronous_commit TO OFF");
 	res = PQexec(conn, sqlquery);
 	if (PQresultStatus(res) != PGRES_COMMAND_OK)
 	{