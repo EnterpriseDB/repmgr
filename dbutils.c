EnterpriseDB/repmgr

commit 2bc8044fdabcd520cf5cc94ba90005bfad8d5072

Improve messages in wait_connection_availability, so we know what error makes the failover procedure to start
By gripe from Andres Freund

dbutils.c
@@ -409,7 +409,7 @@ wait_connection_availability(PGconn *conn, int timeout)
 	{
 		if (PQconsumeInput(conn) == 0)
 		{
-			log_warning(_("PQconsumeInput: Query could not be sent to primary. %s\n"),
+			log_warning(_("wait_connection_availability: could not receive data from master. %s\n"),
 			            PQerrorMessage(conn));
 			return 0;
 		}
@@ -425,8 +425,10 @@ wait_connection_availability(PGconn *conn, int timeout)
 	}
 	if (timeout >= 0)
 		return 1;
-	else
+	else {
+		log_warning(_("wait_connection_availability: timeout reached");
 		return -1;
+	}
 }
 
 

commit ad3630e7a93d2be3113d2605dbfb74ce41ec5da0

Add a missing ')'. This is a typo introduced in commit 2bc8044fdabcd520cf5cc94ba90005bfad8d5072
Per complaint from Carlos Chapi when compiling for a customer.

dbutils.c
@@ -426,7 +426,7 @@ wait_connection_availability(PGconn *conn, int timeout)
 	if (timeout >= 0)
 		return 1;
 	else {
-		log_warning(_("wait_connection_availability: timeout reached");
+		log_warning(_("wait_connection_availability: timeout reached"));
 		return -1;
 	}
 }