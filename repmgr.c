167b4efbb331b1a276ecaeb5b373df4e49aad962
Ian Barwick <ian@2ndquadrant.com>
2015-12-22 10:47:44 +0900

    Add note about why 'hot_standby=on' is currently required

--- repmgr.c
+++ repmgr.c
@@ -4262,7 +4262,6 @@ check_upstream_config(PGconn *conn, int server_version_num, bool exit_on_error)
 	 * of what's in 'archive_command', so until 'archive_mode' is on we can't
 	 * properly check it.
 	 */
-
 	if (guc_set(conn, "archive_mode", "=", "on"))
 	{
 		i = guc_set(conn, "archive_command", "!=", "");
@@ -4283,6 +4282,11 @@ check_upstream_config(PGconn *conn, int server_version_num, bool exit_on_error)
 	}
 
 
+	/*
+	 * Check that 'hot_standby' is on. This isn't strictly necessary
+	 * for the primary server, however the assumption is that configuration
+	 * should be consistent for all servers in a cluster.
+	 */
 	i = guc_set(conn, "hot_standby", "=", "on");
 	if (i == 0 || i == -1)
 	{
