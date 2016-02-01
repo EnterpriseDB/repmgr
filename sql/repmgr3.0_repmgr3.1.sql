c9c18d62164b1c431b963b28625541988e017737
Ian Barwick <ian@2ndquadrant.com>
2016-02-01 13:49:58 +0900

    Update comments in SQL upgrade file

--- sql/repmgr3.0_repmgr3.1.sql
+++ sql/repmgr3.0_repmgr3.1.sql
@@ -2,32 +2,34 @@
  * Update a repmgr 3.0 installation to repmgr 3.1
  * ----------------------------------------------
  *
- * 1. Stop any running repmgrd instances
- * 2. On the master node, execute the SQL statements listed below,
- *    taking care to identify the master node and any inactive
- *    nodes
- * 3. Restart repmgrd (being sure to use repmgr 3.1)
+ * The new repmgr package should be installed first. Then
+ * carry out these steps:
+ *
+ *   1. (If repmgrd is used) stop any running repmgrd instances
+ *   2. On the master node, execute the SQL statements listed below
+ *   3. (If repmgrd is used) restart repmgrd
  */
 
 /*
- * Set the search path to the name of the schema used by
- * your repmgr installation
- * (this should be "repmgr_" + the cluster name defined in
- * 'repmgr.conf')
+ * If your repmgr installation is not included in your repmgr
+ * user's search path, please set the search path to the name
+ * of the repmgr schema to ensure objects are installed in
+ * the correct location.
+ *
+ * The repmgr schema is  "repmgr_" + the cluster name defined in
+ * 'repmgr.conf'.
  */
 
 -- SET search_path TO 'name_of_repmgr_schema';
 
 BEGIN;
 
--- We have this new view which gives the upstream node information when
--- checking on the nodes table
+-- New view "repl_show_nodes" which also displays the server's
+-- upstream node
 
-CREATE VIEW repl_show_nodes AS 
+CREATE VIEW repl_show_nodes AS
 SELECT rn.id, rn.conninfo, rn.type, rn.name, rn.cluster,
 	rn.priority, rn.active, sq.name AS upstream_node_name
 FROM repl_nodes as rn LEFT JOIN repl_nodes AS sq ON sq.id=rn.upstream_node_id;
 
 COMMIT;
-
-
