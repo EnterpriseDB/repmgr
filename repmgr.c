EnterpriseDB/repmgr

commit 85f68e9f77bb6aa0a666e88cb2ce3b06cbcda583

Only collect remote command output if the caller requires it
This addresses GitHub #216 and #167.

repmgr.c
@@ -3526,7 +3526,7 @@ do_standby_switchover(void)
 		remote_host,
 		runtime_options.remote_user,
 		command,
-		&command_output);
+		NULL);
 
 	termPQExpBuffer(&command_output);
 
@@ -5956,15 +5956,18 @@ remote_command(const char *host, const char *user, const char *command, PQExpBuf
 		return false;
 	}
 
-	/* TODO: better error handling */
-	while (fgets(output, MAXLEN, fp) != NULL)
+	if (outputbuf != NULL)
 	{
-		appendPQExpBuffer(outputbuf, "%s", output);
+		/* TODO: better error handling */
+		while (fgets(output, MAXLEN, fp) != NULL)
+		{
+			appendPQExpBuffer(outputbuf, "%s", output);
+		}
 	}
-
 	pclose(fp);
 
-	log_verbose(LOG_DEBUG, "remote_command(): output returned was:\n%s", outputbuf->data);
+	if (outputbuf != NULL)
+		log_verbose(LOG_DEBUG, "remote_command(): output returned was:\n%s", outputbuf->data);
 
 	return true;
 }

commit 9a07686ceb0545020c13b948252e41830ec2db04

When the output of a remote command isn't required, ensure it's consumed anyway
This fixes a regression introduced with commit 85f68e9f77bb6aa0a666e88cb2ce3b06cbcda583

Also clean up some code made redundant by same.

repmgr.c
@@ -3526,15 +3526,11 @@ do_standby_switchover(void)
 
 	log_debug("Executing:\n%s\n", command);
 
-	initPQExpBuffer(&command_output);
-
 	(void)remote_command(
 		remote_host,
 		runtime_options.remote_user,
 		command,
 		NULL);
-
-	termPQExpBuffer(&command_output);
 
 	/* verify that new standby is connected and replicating */
 
@@ -5999,6 +5995,22 @@ remote_command(const char *host, const char *user, const char *command, PQExpBuf
 			appendPQExpBuffer(outputbuf, "%s", output);
 		}
 	}
+	else
+	{
+		/*
+		 * When executed remotely, repmgr commands which execute pg_ctl (particularly
+		 * `repmgr standby follow`) will see the pg_ctl command appear to fail with a
+		 * non-zero return code when the output from the executed pg_ctl command
+		 * has nowhere to go, even though the command actually succeeds. We'll consume an
+		 * arbitrary amount of output and throw it away to work around this.
+		 */
+		int i = 0;
+		while (fgets(output, MAXLEN, fp) != NULL && i < 10)
+		{
+			i++;
+		}
+	}
+
 	pclose(fp);
 
 	if (outputbuf != NULL)