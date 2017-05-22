bc9991df1117fc6ac6fb02d071503d15a0b27932
Ian Barwick <ian@2ndquadrant.com>
2017-05-22 11:53:59 +0900

    Add basic regression test from downstream
    Per GitHub #282
    Ref: https://anonscm.debian.org/cgit/pkg-postgresql/repmgr.git/tree/debian/patches/regress.patch

---
+++ sql/repmgr_test.sql
@@ -0,0 +1,4 @@
+select * from repmgr_update_standby_location('');
+select * from repmgr_get_last_standby_location();
+select * from repmgr_update_last_updated();
+select * from repmgr_get_last_updated();
