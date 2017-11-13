7fffe3ed965914aeb65ac8e7d3a8c25bf5840372
Ian Barwick <ian@2ndquadrant.com>
2017-11-13 10:21:31 +0900

    witness: initial code framework

---
+++ repmgr-action-witness.c
@@ -0,0 +1,47 @@
+/*
+ * repmgr-action-witness.c
+ *
+ * Implements witness actions for the repmgr command line utility
+ *
+ * Copyright (c) 2ndQuadrant, 2010-2017
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <sys/stat.h>
+
+#include "repmgr.h"
+#include "dirutil.h"
+#include "compat.h"
+#include "controldata.h"
+
+#include "repmgr-client-global.h"
+#include "repmgr-action-witness.h"
+
+void
+do_witness_register(void)
+{
+	return;
+}
+
+void
+do_witness_unregister(void)
+{
+	return;
+}
+
+void do_witness_help(void)
+{
+	return;
+}
+
