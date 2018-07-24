c236405251ebb09bc2e9b7414bc082c4e93754dd
Ian Barwick <ian@2ndquadrant.com>
2018-07-24 09:31:37 +0900

    Update extension metadata for 4.1 release
    This release does not make any changes to the extension database
    objects.

--- repmgr--4.0-4.1.sql
+++
@@ -1,8 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION repmgr" to load this file. \quit
-
-ALTER TABLE repmgr.nodes
-  DROP CONSTRAINT nodes_type_check,
-  ADD CONSTRAINT nodes_type_check CHECK (type IN('primary','standby','witness','bdr','bdr_standby'));
-
- type             TEXT        NOT NULL CHECK (type IN('primary','standby','witness','bdr','bdr_standby')),
