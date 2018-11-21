9d1f5c0de31729cff25d82e9bad75586353f31d1
Ian Barwick <ian@2ndquadrant.com>
2018-11-21 12:39:27 +0900

    Update 4.2 - 4.3 extension upgrade script

--- repmgr--4.2--4.3.sql
+++ repmgr--4.2--4.3.sql
@@ -1,4 +1,12 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION repmgr" to load this file. \quit
 
--- XXX add new functions here
+CREATE FUNCTION set_primary_last_seen()
+  RETURNS VOID
+  AS 'MODULE_PATHNAME', 'set_primary_last_seen'
+  LANGUAGE C STRICT;
+
+CREATE FUNCTION get_primary_last_seen()
+  RETURNS INT
+  AS 'MODULE_PATHNAME', 'get_primary_last_seen'
+  LANGUAGE C STRICT;
