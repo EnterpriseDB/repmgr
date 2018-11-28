ae3f302dc2f8e93088af47cf1852ea39423cf9c8
Ian Barwick <ian@2ndquadrant.com>
2018-11-28 12:57:16 +0900

    doc: document "repmgr node service"
    This was originally intended for internal use, but it's mentioned
    several times in the documentation and is useful for diagnostic
    purposes.

--- repmgr-client.c
+++ repmgr-client.c
@@ -29,14 +29,12 @@
  *
  * NODE STATUS
  * NODE CHECK
+ * NODE REJOIN
+ * NODE SERVICE
  *
  * DAEMON STATUS
  * DAEMON PAUSE
  * DAEMON UNPAUSE
- *
- * For internal use:
- * NODE REJOIN
- * NODE SERVICE
  *
  *
  * This program is free software: you can redistribute it and/or modify
