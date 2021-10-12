70c79aeaecad231dc90cee5e749b8f0c9522fd41
Ian Barwick <ian.barwick@enterprisedb.com>
2021-10-12 16:50:33 +0900

    Add dummy include file
    This is a workaround required to facilitate Debian package builds
    against PostgreSQL Extended.

---
+++ bdr_api_version.h
@@ -0,0 +1,7 @@
+/*
+ * bdr_version_api.h
+ *
+ * This is a dummy file to facilitated building Debian packages against
+ * PostgreSQL Extended. It may be removed in the future. Do not add any
+ * code to this file.
+ */
