580d112d3d6030760a6574784de9a976c1bd9b2c
Ian Barwick <ian.barwick@enterprisedb.com>
2021-10-29 15:41:53 +0900

    Removed temporary include file workaround

--- bdr_api_version.h
+++
@@ -1,7 +0,0 @@
-/*
- * bdr_version_api.h
- *
- * This is a dummy file to facilitated building Debian packages against
- * PostgreSQL Extended. It may be removed in the future. Do not add any
- * code to this file.
- */
